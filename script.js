f8fa0c5cc11fd74d2602af5efa546ca4fd6473e1
VANSH GOKHALE <150286641+Vansh-Gokhale@users.noreply.github.com>
2024-03-12 20:11:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+document.getElementById('calculate-btn').addEventListener('click', function() {
+    const height = parseFloat(document.getElementById('height').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+    const bmiUnit = document.querySelector('input[name="bmi"]:checked').value;
+  
+    if (!height || !weight) {
+      alert('Please enter a valid height and weight.');
+      return;
+    }
+  
+    let bmi;
+  
+    if (bmiUnit === 'kgs') {
+      bmi = weight / (height*height);
+    } else {
+      bmi = (weight * 2.2046) / (height * 0.0254) ** 2;
+    }
+  
+    document.getElementById('result').value = bmi.toFixed(2);
+  });
